82b70eb159919361afc065a78544ab26de6b7291
Cedust <t.sudeck@live.de>
2026-02-24 22:23:36 +0100

    buffed epic and legendary runes

--- src/core/constants/runes/rune-quality-specs.config.ts
+++ src/core/constants/runes/rune-quality-specs.config.ts
@@ -19,17 +19,17 @@ export const RUNE_ATTACK_SPEED: RuneQualitySpec[] = [
 export const RUNE_CHANCE: RuneQualitySpec[] = [
   { Quality: 'Common', Value: { Min: 0.01, Max: 0.1, Type: 'Percent' } },
   { Quality: 'Magic', Value: { Min: 0.1, Max: 0.2, Type: 'Percent' } },
-  { Quality: 'Rare', Value: { Min: 0.2, Max: 0.3, Type: 'Percent' } },
-  { Quality: 'Epic', Value: { Min: 0.3, Max: 0.4, Type: 'Percent' } },
-  { Quality: 'Legendary', Value: { Min: 0.4, Max: 0.5, Type: 'Percent' } }
+  { Quality: 'Rare', Value: { Min: 0.2, Max: 0.4, Type: 'Percent' } },
+  { Quality: 'Epic', Value: { Min: 0.4, Max: 0.75, Type: 'Percent' } },
+  { Quality: 'Legendary', Value: { Min: 0.75, Max: 1.0, Type: 'Percent' } }
 ];
 
 export const RUNE_BLEEDING_DAMAGE: RuneQualitySpec[] = [
   { Quality: 'Common', Value: { Min: 0.01, Max: 0.1, Type: 'Percent' } },
   { Quality: 'Magic', Value: { Min: 0.1, Max: 0.2, Type: 'Percent' } },
   { Quality: 'Rare', Value: { Min: 0.2, Max: 0.35, Type: 'Percent' } },
-  { Quality: 'Epic', Value: { Min: 0.35, Max: 0.5, Type: 'Percent' } },
-  { Quality: 'Legendary', Value: { Min: 0.5, Max: 0.75, Type: 'Percent' } }
+  { Quality: 'Epic', Value: { Min: 0.35, Max: 0.6, Type: 'Percent' } },
+  { Quality: 'Legendary', Value: { Min: 0.6, Max: 1.25, Type: 'Percent' } }
 ];
 
 export const RUNE_CRIT_DAMAGE: RuneQualitySpec[] = [
@@ -37,7 +37,7 @@ export const RUNE_CRIT_DAMAGE: RuneQualitySpec[] = [
   { Quality: 'Magic', Value: { Min: 0.25, Max: 0.5, Type: 'Percent' } },
   { Quality: 'Rare', Value: { Min: 0.5, Max: 0.75, Type: 'Percent' } },
   { Quality: 'Epic', Value: { Min: 0.75, Max: 1.0, Type: 'Percent' } },
-  { Quality: 'Legendary', Value: { Min: 1.0, Max: 1.5, Type: 'Percent' } }
+  { Quality: 'Legendary', Value: { Min: 1.0, Max: 2.0, Type: 'Percent' } }
 ];
 
 export const RUNE_MULTI_HIT_DAMAGE: RuneQualitySpec[] = [
@@ -45,7 +45,7 @@ export const RUNE_MULTI_HIT_DAMAGE: RuneQualitySpec[] = [
   { Quality: 'Magic', Value: { Min: 0.12, Max: 0.25, Type: 'Percent' } },
   { Quality: 'Rare', Value: { Min: 0.25, Max: 0.5, Type: 'Percent' } },
   { Quality: 'Epic', Value: { Min: 0.5, Max: 0.75, Type: 'Percent' } },
-  { Quality: 'Legendary', Value: { Min: 0.75, Max: 1.0, Type: 'Percent' } }
+  { Quality: 'Legendary', Value: { Min: 0.75, Max: 1.5, Type: 'Percent' } }
 ];
 
 export const RUNE_MULTI_HIT_CHAIN: RuneQualitySpec[] = [
@@ -77,5 +77,5 @@ export const RUNE_CHARGE_DAMAGE: RuneQualitySpec[] = [
   { Quality: 'Magic', Value: { Min: 0.12, Max: 0.25, Type: 'Percent' } },
   { Quality: 'Rare', Value: { Min: 0.25, Max: 0.5, Type: 'Percent' } },
   { Quality: 'Epic', Value: { Min: 0.5, Max: 0.75, Type: 'Percent' } },
-  { Quality: 'Legendary', Value: { Min: 0.75, Max: 1.0, Type: 'Percent' } }
+  { Quality: 'Legendary', Value: { Min: 0.75, Max: 1.5, Type: 'Percent' } }
 ];
